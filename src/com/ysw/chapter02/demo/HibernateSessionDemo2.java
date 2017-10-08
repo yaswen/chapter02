3ceb7ae3bb6751cc36bfd880287899d65213ae73
yaswen <yaswen@163.com>
2017-10-08 23:53:05 +0800

    批量加1万条user

--- src/com/ysw/chapter02/demo/HibernateSessionDemo2.java
+++ src/com/ysw/chapter02/demo/HibernateSessionDemo2.java
@@ -1,5 +1,7 @@
 package com.ysw.chapter02.demo;
 
+import java.util.Random;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import com.ysw.chapter02.pojos.User;
@@ -10,18 +12,48 @@ public class HibernateSessionDemo2 {
 	public static void main(String[] args){
 		//addus();
 		//updateuns();
+		//
+		add1w();
 		
 	}
-	//批量加一下数据用
+	public static void add1w() {
+		Session session=HibernateUtils.getSession();
+		Transaction trans=session.beginTransaction();
+		/*保存1万个User对象*/
+		for(int i=0;i<10000;i++) {
+			User user=new User("shiwen"+nonce(4),"p"+nonce(5),i%100);
+			session.save(user);
+			if(i%10==0) {
+				session.flush();
+				session.clear();
+				trans.commit();
+				trans=session.beginTransaction();
+			}
+		}
+		HibernateUtils.closeSession();
+	}
+	//随机6位字符串用
+	public static String nonce(int wei) {
+		String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+		Random random = new Random();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < wei; i++) {
+			int number = random.nextInt(base.length());
+			sb.append(base.charAt(number));
+		}
+		return sb.toString();
+	}
+	//shiwen批量加一下数据用
 	public static void addus() {
 		//批量加一下数据
+		
 				for(int i = 1 ; i<=10;i++) {
 					User user= new User("yas"+i,"111"+i,i);
 					System.out.println("新建用户对象"+" "+user.getUserName()+" "+user.getUserPwd()+" "+user.getRole());
 					addUser(user);
 				}
 	}
-	/*shiwen批量修改用户名*/
+	//shiwen批量修改用户名用
 	public static void updateuns() {
 		for(int i = 1 ;i<=10;i++){
 			User user= getUser(new Integer(i));
